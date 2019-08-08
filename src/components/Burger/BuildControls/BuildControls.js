21b6e73badebe9b03928f35b4cf5a9a4be8dd8ea
Roger Chien <pshadow.tw@gmail.com>
2019-08-08 00:20:27 -0700

    add functions for "add" and "remove" ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,8 +11,15 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        {controls.map(control => 
-            <BuildControl key={control.label} label={control.label} />
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(control =>
+            <BuildControl
+                key={control.label}
+                label={control.label}
+                added={() => props.ingredientAdded(control.type)}
+                removed={() => props.ingredientRemoved(control.type)}
+                disabled={props.disabledInfo[control.type]}
+            />
         )}
     </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.7,
+    cheese: 0.4,
+    meat: 1.3
+};
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -9,13 +16,40 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        const updatedIngredients = { ...this.state.ingredients };
+        updatedIngredients[type] = this.state.ingredients[type] + 1;
+        const updatedTotalPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({ ingredients: updatedIngredients, totalPrice: updatedTotalPrice });
+    };
+
+    removeIngredientHandler = (type) => {
+        const updatedIngredients = { ...this.state.ingredients };
+        if (this.state.ingredients[type] <= 0) return;
+        updatedIngredients[type] = this.state.ingredients[type] - 1;
+        const updatedTotalPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+        this.setState({ ingredients: updatedIngredients, totalPrice: updatedTotalPrice });
+    };
+
     render() {
+        const disabledInfo = { ...this.state.ingredients }
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        };
+        // { salad: true, meat: false...}
         return (
             <React.Fragment>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabledInfo={disabledInfo}
+                    price={this.state.totalPrice}
+                />
             </React.Fragment>
         )
     }
