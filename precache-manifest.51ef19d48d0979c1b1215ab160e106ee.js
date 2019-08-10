eef8c7882625b442721647ccdd61c64422c57c59
Roger Chien <pshadow.tw@gmail.com>
2019-08-10 00:47:37 -0700

    Updates

---
+++ precache-manifest.51ef19d48d0979c1b1215ab160e106ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08a3c571d7371855c72f9fda41f8404",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "5da1ad677a26f1d8412e",
+    "url": "/BurgerBuilder/static/css/main.497ff5f1.chunk.css"
+  },
+  {
+    "revision": "aecd56cd4f395d5ad7f2",
+    "url": "/BurgerBuilder/static/js/2.17fe79c6.chunk.js"
+  },
+  {
+    "revision": "5da1ad677a26f1d8412e",
+    "url": "/BurgerBuilder/static/js/main.45cc7068.chunk.js"
+  },
+  {
+    "revision": "c4803fff7a346a4d9e6f",
+    "url": "/BurgerBuilder/static/js/runtime~main.70232762.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
