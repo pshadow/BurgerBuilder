941186abf819ec3c39d48b4226e3757eeeb37387
Roger Chien <roger.chien@innovyze.com>
2019-12-10 11:16:59 -0800

    fix layout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -23,7 +23,7 @@ class Checkout extends Component {
                     checkoutContinued={this.checkoutContinuedHandler} />
                 <Route
                     path={this.props.match.path + '/contact-data'}
-                    component = {ContactData} />
+                    component={ContactData} />
             </div>
         )
     }
