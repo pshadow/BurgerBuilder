fac402607050ee337f4db7ef8410ab4b301efb5c
Roger Chien <roger.chien@innovyze.com>
2019-12-12 12:57:49 -0800

    use "object shorthand" form of mapDispatchToProps

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,21 +7,26 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Modal from '../../components/UI/Modal/Modal';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actions from '../../store/actions/index';
+import {
+    addIngredient,
+    initIngredients,
+    purchaseInit,
+    removeIngredient
+} from '../../store/actions/index';
+
 class BurgerBuilder extends Component {
     state = {
         purchasing: false
-    }
+    };
 
     componentDidMount() {
-        this.props.onInitIngredients();
+        this.props.initIngredients();
     }
 
     updatedOrderNowDisabled = ingredients =>
         Object.keys(ingredients)
             .map(igKey => ingredients[igKey])
-            .reduce((sum, el) => sum + el, 0)
-        <= 0;
+            .reduce((sum, el) => sum + el, 0) <= 0;
 
     orderNowClickedHandler = () => {
         this.setState({ purchasing: true });
@@ -32,46 +37,58 @@ class BurgerBuilder extends Component {
     };
 
     orderContinueHandler = () => {
-        this.props.onInitPurchase();
+        this.props.purchaseInit();
         this.props.history.push('/checkout');
     };
 
     render() {
-        const disabledInfo = { ...this.props.ings }
+        const disabledInfo = { ...this.props.ings };
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0;
-        };
+        }
         // { salad: true, meat: false...}
         let orderSummary = null;
-        let burger = this.props.error ? <p>Burger Ingredients Cannot be loadded</p> : <Spinner />;
+        let burger = this.props.error ? (
+            <p>Burger Ingredients Cannot be loadded</p>
+        ) : (
+            <Spinner />
+        );
         if (this.props.ings) {
             burger = (
                 <React.Fragment>
                     <Burger ingredients={this.props.ings} />
                     <BuildControls
-                        ingredientAdded={this.props.onIngredientAdded}
-                        ingredientRemoved={this.props.onIngredientRemoved}
+                        ingredientAdded={this.props.addIngredient}
+                        ingredientRemoved={this.props.removeIngredient}
                         disabledInfo={disabledInfo}
                         price={this.props.price}
-                        orderNowDisabled={this.updatedOrderNowDisabled(this.props.ings)}
+                        orderNowDisabled={this.updatedOrderNowDisabled(
+                            this.props.ings
+                        )}
                         orderNowClicked={this.orderNowClickedHandler}
                     />
                 </React.Fragment>
             );
-            orderSummary = <OrderSummary
-                ingredients={this.props.ings}
-                price={this.props.price}
-                cancel={this.orderCanceledHandler}
-                continue={this.orderContinueHandler} />;
+            orderSummary = (
+                <OrderSummary
+                    ingredients={this.props.ings}
+                    price={this.props.price}
+                    cancel={this.orderCanceledHandler}
+                    continue={this.orderContinueHandler}
+                />
+            );
         }
         return (
             <React.Fragment>
-                <Modal show={this.state.purchasing} backdropClicked={this.orderCanceledHandler}>
+                <Modal
+                    show={this.state.purchasing}
+                    backdropClicked={this.orderCanceledHandler}
+                >
                     {orderSummary}
                 </Modal>
                 {burger}
             </React.Fragment>
-        )
+        );
     }
 }
 
@@ -80,16 +97,17 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
         error: state.burgerBuilder.error
-    }
+    };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
-        onInitIngredients: () => dispatch(actions.initIngredients()),
-        onInitPurchase: () => dispatch(actions.purchaseInit())
-    }
+const mapDispatchToProps = {
+    addIngredient,
+    removeIngredient,
+    initIngredients,
+    purchaseInit
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withErrorHandler(BurgerBuilder, axios));
