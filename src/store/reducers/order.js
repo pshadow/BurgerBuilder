3613fade69f9db9f0a33a0e5d6c369758d1e696f
Roger Chien <roger.chien@innovyze.com>
2019-12-11 17:12:38 -0800

    add utility function "updateObject()" and use it

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,65 +1,44 @@
-import * as actionTypes from '../actions/actionsTypes'
+import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     orders: [],
     loading: false,
     purchased: false
-}
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            }
-
+            return updateObject(state, { loading: true });
         case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
+            const newOrder = updateObject(action.orderData, {
                 id: action.orderId
-            }
-            return {
-                ...state,
+            });
+            return updateObject(state, {
                 loading: false,
                 purchased: true,
                 orders: state.orders.concat(newOrder)
-            }
-
+            });
         case actionTypes.PURCHASE_BURGER_FAILED:
-            return {
-                ...state,
+            return updateObject(state, {
                 loading: false,
                 error: action.error
-            }
-
+            });
         case actionTypes.PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false
-            }
-
+            return updateObject(state, { loading: false });
         case actionTypes.FETCH_ORDERS_START:
-            return {
-                ...state,
-                loading: true
-            }
-
+            return updateObject(state, { loading: true });
         case actionTypes.FETCH_ORDERS_SUCCESS:
-            return {
-                ...state,
+            return updateObject(state, {
                 loading: false,
                 orders: action.orders
-            }
+            });
         case actionTypes.FETCH_ORDERS_FAIL:
-            return {
-                ...state,
-                loading: false
-            }
-
+            return updateObject(state, { loading: false });
         default:
             return state;
     }
-}
+};
 
 export default reducer;
